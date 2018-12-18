fb5606a742c56e4ed9f1267fa880e3e1f39214c0
200367n <200367n@ugs.kochi-tech.ac.jp>
2018-12-18 17:38:25 +0900

    Update putReview.php

--- htdocs/putReview.php
+++ htdocs/putReview.php
@@ -1,33 +1,33 @@
 <?php
+function putReview() {
+  require "database.php";
 
-require "database.php";
+  // タイムゾーン設定
+  date_default_timezone_set('Asia/Tokyo');
 
-// タイムゾーン設定
-date_default_timezone_set('Asia/Tokyo');
+  if (isset($_GET['reviewPoint'])) {
+    $reviewPoint = $_GET['reviewPoint'];
+  }
 
-if (isset($_GET['reviewPoint'])) {
-  $reviewPoint = $_GET['reviewPoint'];
+  // 入力されたコメントと受信時刻を取得
+  if (isset($_GET['reviewComment'])) {
+    $reviewComment = $_GET['reviewComment'];
+    $timestamp = time();
+  }
+
+  // database.phpに投げるSQL文
+  database("INSERT", "INTO review VALUES (id, itemid, $reviewComment, replyid, $reviewPoint, $timestamp");
+
+  // SQL文
+  /*$sql = " INSERT INTO review VALUES (id, itemid, $reviewComment, replyid, $reviewPoint, $timestamp) ";
+  if ($result = $mysqli -> query($sql)) {
+
+    $result -> close();
+  } else {
+    exit(' 送信に失敗しました ');
+  }*/
+  // Database終了
+
+  $mysqli -> close();
 }
-
-// 入力されたコメントと受信時刻を取得
-if (isset($_GET['reviewComment'])) {
-  $reviewComment = $_GET['reviewComment'];
-  $timestamp = time();
-}
-
-// database.phpに投げるSQL文
-database("INSERT", "INTO review VALUES (id, itemid, $reviewComment, replyid, $reviewPoint, $timestamp");
-
-// SQL文
-/*$sql = " INSERT INTO review VALUES (id, itemid, $reviewComment, replyid, $reviewPoint, $timestamp) ";
-if ($result = $mysqli -> query($sql)) {
-
-  $result -> close();
-} else {
-  exit(' 送信に失敗しました ');
-}*/
-// Database終了
-
-$mysqli -> close();
-
 ?>
