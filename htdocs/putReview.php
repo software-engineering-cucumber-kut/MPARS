b492e912a8f61a5eb4d9f0f7cfda4b1d2408689a
200367n <200367n@ugs.kochi-tech.ac.jp>
2018-12-18 16:08:39 +0900

    Update putReview.php

--- htdocs/putReview.php
+++ htdocs/putReview.php
@@ -1,3 +1,26 @@
 <?php
+// タイムゾーン設定
+date_default_timezone_set('Asia/Tokyo');
+
+if (isset($_GET['reviewPoint'])) {
+  $reviewPoint = $_GET['reviewPoint'];
+}
+
+// 入力されたコメントと受信時刻を取得
+if (isset($_GET['reviewComment'])) {
+  $reviewComment = $_GET['reviewComment'];
+  $timestamp = time();
+}
+
+
+// SQL文
+$sql = " INSERT INTO review VALUES (id, itemid, $reviewComment, replyid, $reviewPoint, $timestamp) ";
+if ($result = $mysqli -> query($sql)) {
+  $result -> close();
+} else {
+  exit(' 送信に失敗しました ');
+}
+// Database終了
+$mysqli -> close();
 
 ?>
