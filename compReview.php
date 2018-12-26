9eb504cf3bd0deddd8d50d222c0fa3c9bc1ddecc
mikarinn <200326q@ugs.kochi-tech.ac.jp>
2018-12-26 23:51:43 +0900

    add compReview.php

---
+++ compReview.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<head>
+    <meta charset="utf-8">
+    <script type="text/javascript" src="compReview.js"></script>
+    <link rel="stylesheet" type="text/css" href="compReview.css">
+    <title>MPARS</title>
+</head>
+
+<body>
+    <!-- Start your code here -->
+
+    <!-- ここにheader.phpを挿入 -->
+
+    <center>
+        <p>レビューが投稿されました。</p>
+        <input type="button" value="戻る" onclick="back()">
+    </center>
+    
+    <!-- End your code here -->
+</body>
+
+</html>
