5fa9c8724a1ed01f75989dd3990895b482dee399
mikarinn <200326q@ugs.kochi-tech.ac.jp>
2018-12-27 18:18:12 +0900

    update compReview.php

--- compReview.php
+++ compReview.php
@@ -1,12 +1,20 @@
 <!DOCTYPE html>
 <html lang="ja">
 
-<head>
-    <meta charset="utf-8">
-    <script type="text/javascript" src="compReview.js"></script>
-    <link rel="stylesheet" type="text/css" href="compReview.css">
-    <title>MPARS</title>
-</head>
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MPARS</title>
+        <script type="text/javascript" src="compReview.js"></script>
+        <link rel="stylesheet" type="text/css" href="compReview.css">
+        <!-- BootstrapのCSS読み込み -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- jQuery読み込み -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- BootstrapのJS読み込み -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
 
 <body>
     <!-- Start your code here -->
@@ -15,7 +23,7 @@
 
     <center>
         <p>レビューが投稿されました。</p>
-        <input type="button" value="戻る" onclick="back()">
+        <button type="button" class="btn btn-primary" onclick="back()">戻る</button>
     </center>
     
     <!-- End your code here -->
