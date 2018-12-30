779fcf56c3fb9e9d30196bfc7f4cfdee4c0c3dd1
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2018-12-31 05:23:04 +0900

    add session management to putProduct.php

--- htdocs/putProduct.php
+++ htdocs/putProduct.php
@@ -1,11 +1,16 @@
 <?php
-require('database.php');
-$json_string = file_get_contents('php://input');
-$json_obj = json_decode($json_string);
-$target_string = 'INTO item (id, name, photo, description, price, storeid) VALUES (NULL, '.
-    "'" . $json_obj->name . "'" . ',' .
-    "'" . $json_obj->photo . "'" . ',' .
-    "'" . $json_obj->description . "'" . ',' .
-    $json_obj->price . ',' .
-    $json_obj->storeid . ')';
-database('INSERT', $target_string);
+session_start();
+if (isset($_SESSION['id'])) {
+    require('database.php');
+    $json_string = file_get_contents('php://input');
+    $json_obj = json_decode($json_string);
+    $target_string = 'INTO item (id, name, photo, description, price, storeid) VALUES (NULL, '.
+        "'" . $json_obj->name . "'" . ',' .
+        "'" . $json_obj->photo . "'" . ',' .
+        "'" . $json_obj->description . "'" . ',' .
+        $json_obj->price . ',' .
+        $json_obj->storeid . ')';
+    database('INSERT', $target_string);
+} else {
+    echo "missing session";
+}
