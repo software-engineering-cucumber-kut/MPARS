b476e9694474806f5c50112c8be7383418ded166
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-02 01:40:17 +0900

    fix putProduct.php (missing return value)

--- htdocs/putProduct.php
+++ htdocs/putProduct.php
@@ -3,6 +3,7 @@
 session_start();
 if (isset($_SESSION['id'])) {
     require('database.php');
+    global $last_insert_id;
     $json_string = file_get_contents('php://input');
     $json_obj = json_decode($json_string);
     $target_string = 'INTO item (id, name, photo, description, price, storeid) VALUES (NULL, '.
