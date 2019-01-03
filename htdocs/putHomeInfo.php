e3c922e2140e4b1f2cc1b67c29d23f0b311f2ef3
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-03 16:19:24 +0900

    add putHomeInfo.php

--- htdocs/putHomeInfo.php
+++ htdocs/putHomeInfo.php
@@ -7,13 +7,15 @@ if (isset($_SESSION['id'])) {
     require('database.php');
     $json_string = file_get_contents('php://input');
     $json_obj = json_decode($json_string);
+
+    // 店舗情報とイベント情報を分ける
     $shopInfos = $json_obj->shopinfo;
-//    var_dump($json_obj); 
-//    var_dump($shopInfos); 
+    $eventInfos = $json_obj->event;
+
+    // 先に店舗情報をDBに格納する
     foreach ($shopInfos as $shopInfo) {
-//        var_dump($shopInfo);
         $shopId = database('SELECT', 'id from store WHERE name = ' . "'" . $shopInfo->name . "'");
-//        var_dump($shopId);
+        //  同名の店がなかった場合
         if ($shopId == NULL) {
 //            echo "the store not found. create new record.\n";
             $target_string = 'INTO store (name, color, phonenumber, address) VALUES (' .
@@ -23,14 +25,15 @@ if (isset($_SESSION['id'])) {
                 "'" . $shopInfo->address . "')";
 //            echo 'Insert this:' . $target_string . "\n";
             $dbaccess_result = database('INSERT', $target_string);
-//            var_dump($last_insert_id);
+            // insert失敗
             if (!$dbaccess_result) {
                 exit("0\n}\n");
             }
+            // 挿入したレコードのidを保管しておく（businessdayで検索するため）
             $shopId = $last_insert_id;
         } else {
+            // 店があった場合．変更したレコードのidを保管しておく
             $shopId = (int)$shopId[0]["id"];
-//            var_dump($shopId);
             $target_string = 'store SET ' .
                 "name = '" . $shopInfo->name . "'," .
                 "color = '" . $shopInfo->color . "'," .
@@ -38,21 +41,9 @@ if (isset($_SESSION['id'])) {
                 "address = '" . $shopInfo->address . "' WHERE id = " . $shopId;
 //            echo "update target:" . $target_string . "\n";
             $dbaccess_result = database('UPDATE', $target_string);
-//            var_dump($dbaccess_result);
         }
-            /*
-        $target_string = 'INTO store (id name, color, phonenumber, address) VALUES (' .
-            "'" . $shopInfo->name . "'," .
-            "'" . $shopInfo->color . "'," .
-            "'" . $shopInfo->phonenumber . "'," .
-            $shopInfo->address . ') ON DUPLICATE KEY UPDATE' .
-            "name = '" . $shopInfo->name . "'," .
-            "color = '" . $shopInfo->color . "'," .
-            "phonenumber = '" . $shopInfo->phonenumber . "'," .
-            "address = " . $shopInfo->address;
-             */
-//        echo "shopId:" . $shopId . "\n";
 
+        // 既に店があった場合はupdate
         $target_string = 'INTO businessday (storeid, businessday, open, close) VALUES (' .
             $shopId . "," .
             "'" . $shopInfo->businessday . "'," .
@@ -66,11 +57,30 @@ if (isset($_SESSION['id'])) {
         if (!$dbaccess_result) {
             exit("0\n}\n");
         }
-//        var_dump($dbaccess_result);
+    }
+
+    foreach ($eventInfos as $eventInfo) {
+        $shopId = database('SELECT', 'id from store WHERE name = ' . "'" . $eventInfo->name . "'");
+        if ($shopId == NULL) {
+            exit("0\n}\n");
+        }
+        $shopId = $shopId[0]["id"];
+        $target_string = 'INTO event (storeid, isopen, date, open, close) VALUES (' .
+            $shopId . ', ' .
+            $eventInfo->isopen . ', ' .
+            "'" . $eventInfo->date . "', " .
+            "'" . $eventInfo->open . "', " .
+            "'" . $eventInfo->close . "')";
+//        echo 'event insertion:' . $target_string . "\n";
+        $dbaccess_result = database('INSERT', $target_string);
+        if (!$dbaccess_result) {
+            exit("0\n}\n");
+        }
     }
     echo "1";
 } else {
     echo "0";
+    echo "missing session\n";
 }
 ?>
 
