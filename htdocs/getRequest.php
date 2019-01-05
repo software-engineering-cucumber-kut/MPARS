e4c2d7a0cfbe3e327fd225b7799b46eb3ba2c0b1
YamaD1 <32477483+YamaD1@users.noreply.github.com>
2019-01-06 00:25:37 +0900

    まだ不完全かも...

---
+++ htdocs/getRequest.php
@@ -0,0 +1,20 @@
+{
+    “request”: [
+<?php
+require('database.php');
+$request = database('SELECT', '* FROM request');
+//配列requestから値(contents,date)を順に取り出し$valに格納
+foreach ($request as $val) {
+ echo"\t\t{\n",
+        "\t\t\t" . '"contents":' . $val["contents"] . ",\n",
+        "\t\t\t" . '"date":' . $val["date"] . "\n";
+        //最後まで行ったときの処理
+    if ($val == end($request)) {
+        print("\t\t}\n");
+    } else {
+        print("\t\t},\n");
+    }
+}
+?>
+    ]
+}
