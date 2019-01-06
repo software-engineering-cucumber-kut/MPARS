7cd06fe9a31826bd5c69ec4ae2825e48731e6186
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-07 02:11:51 +0900

    Update getCalendar.php

--- htdocs/getCalendar.php
+++ htdocs/getCalendar.php
@@ -4,12 +4,12 @@
   // タイムゾーン設定
   date_default_timezone_set('Asia/Tokyo');
   // 以降製作途中
-  $calenderInfo = database('SELECT', 'store.color, businessday.businessday FROM store JOIN businessday ON store.id = businessday.storeid');
-  foreach($calenderInfo as $val) {
+  $calendarInfo = database('SELECT', 'store.color, businessday.businessday FROM store JOIN businessday ON store.id = businessday.storeid');
+  foreach($calendarInfo as $val) {
     echo"\t\t{\n",
     "\t\t\t" . '"color":' . $val["color"] . ",\n",
     "\t\t\t" . '"businessday":' . $val["businessday"] . ",\n";
-    if ($val == end($calenderInfo)) {
+    if ($val == end($calendarInfo)) {
         print("\t\t}\n");
     } else {
         print("\t\t},\n");
