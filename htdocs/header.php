ac568dca5b3c37bf4f0659bdf945e9168752f284
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-07 15:51:02 +0900

    add footer.php, header.php, request.php

---
+++ htdocs/header.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MPARS</title>
+
+        <!-- Bootstrapのための各種cssファイル読み込み -->
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
+
+        <link rel="stylesheet" href="css/header.css">
+    </head>
+<body>
+<!-- global menu -->
+<div class="d-flex flex-row">
+<div class="header-img"><img src="img/photo.jpg"><img src="img/photo.jpg"><img src="img/photo.jpg"></div>
+</div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light"> <a class="navbar-brand" href="/"> <img class="logo-img" src="img/logo.svg"> </a>
+  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria　expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
+  <div class="collapse navbar-collapse menu" id="navbarSupportedContent">
+    <ul class="navbar-nav">
+        <li class="nav-item"> <a class="nav-link" href="/">Home<br/>
+        <span class="menu-japan">ホーム</span></a> </li>
+      <li class="nav-item"> <a class="nav-link" href="/menu.php">Menu<br/>
+        <span class="menu-japan">商品一覧</span></a> </li>
+      <li class="nav-item"> <a class="nav-link" href="/request.php">Request<br/>
+        <span class="menu-japan">ご要望</span></a> </li>
+      <li class="nav-item"> <a class="nav-link" href="#">Reservation<br/>
+        <span class="menu-japan">ご予約</span></a> </li>
+    </ul>
+  </div>
+</nav>
---
+++ htdocs/request.php
@@ -0,0 +1,22 @@
+<?php require('header.php');?>
+<div class="container-fluid">
+  <br>
+  <div class="row text-left">
+    <div class="col-md-12">
+      <h4>Request</h4>
+      <hr>
+    </div>
+  </div>
+  <br>
+  ここでは、商品のリクエストやミヤノクチテラスへの要望が投稿できます。
+  <br>
+  <form>
+    <div class="form-group">
+      <textarea class="form-control" rows="3">テキストを入力してください。</textarea>
+    </div>
+    <div class="mx-auto" style="width: 1vw">
+      <button type="submit" class="btn btn-default">送信</button>
+    </div>
+  </form>
+</div>
+<?php require('footer.php');?>
