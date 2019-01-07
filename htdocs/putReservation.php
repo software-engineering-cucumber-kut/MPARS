b81fb42212c81c5e822fe0bd9429eeecdd8965e8
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-07 13:51:21 +0900

    Add putReservation.php

---
+++ htdocs/putReservation.php
@@ -0,0 +1,16 @@
+{
+  "success":<?php
+  session_start();
+  if (isset($_SESSION['id'])) {
+    require('database.php');
+    $json_string = file_get_contents('php://input');
+    $json_obj = json_decode($json_string);
+    $target_string = 'INTO reservation (id, userid, recieved, reservationtime, receiveday) VALUES (NULL, ' .  "'" . $json_obj->userid . "'" . ',' . "'" . $json_obj->received . "'" . ',' . "'" . $json_obj->reservationtime . "'" . ',' . "'" . $json_obj->receiveday . ')';
+    database('INSERT', $target_string);
+    echo "1";
+    } else {
+    echo "0";
+    }  
+  }
+ ?>
+}
