3721c61df8d9aaaaf1e1765fe0855dca5e8ed9e8
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-07 16:59:02 +0900

    move jQuery to header

--- htdocs/header.php
+++ htdocs/header.php
@@ -10,6 +10,12 @@
         <!-- Bootstrapのための各種cssファイル読み込み -->
         <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
+
+        <!-- jQueryのjsファイル読み込み -->
+        <script
+              src="https://code.jquery.com/jquery-3.3.1.min.js"
+              integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
+              crossorigin="anonymous"></script>
         <link rel="stylesheet" href="css/header.css">
     </head>
 <body>
