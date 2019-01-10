d3ebc72ab17bda38d5c8bc18c82cc263f021ad3d
tatsuki sato <imperially9@gmail.com>
2019-01-10 18:44:17 +0900

    add deleteReservation.php

---
+++ deleteReservation.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <title>MPARS</title>
+        <script type="text/javascript" src="deleteReservation.js"></script>
+        <link rel="stylesheet" type="text/css" href="deleteReservation.css">
+        <!-- BootstrapのCSS読み込み -->
+        <link href="css/bootstrap.min.css" rel="stylesheet">
+        <!-- jQuery読み込み -->
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+        <!-- BootstrapのJS読み込み -->
+        <script src="js/bootstrap.min.js"></script>
+    </head>
+
+<body>
+    <!-- Start your code here -->
+
+    <center>
+        <p>予約が取り消されました.</p>
+        <button type="button" class="btn btn-primary" onclick="back()">戻る</button>
+    </center>
+
+    <!-- End your code here -->
+</body>
+
+</html>
