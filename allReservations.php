43b553bfc7f6faf16376369a63b8d5a91f79d2e8
tatsuki sato <imperially9@gmail.com>
2019-01-11 00:43:26 +0900

    update allReservations.php

--- allReservations.php
+++ allReservations.php
@@ -1,42 +1,30 @@
 <!DOCTYPE html>
-<html lang="ja">
-  <head>
-    <meta charset="UTF-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+<html lang ="ja">
 
-    <title>MPARS</title>
+<head>
+  <meta charset="utf-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>MPARS</title>
+  <!--jsとcssの読み込み-->
+  <script type="text/javascript" src="allReservations.js"></script>
+  <script type="text/javascript" src="getAllReservation.js"></script>
+  <link rel="stylesheet" type="text/css" href="allReservations.css">
 
-    <!-- cssの読み込み -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
-    <link href="css/allReservations.css" rel="stylesheet">
-    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-
-    <!-- jsの読み込み -->
-    <script src="js/jquery-3.3.1.min.js"></script>
-    <script src="js/bootstrap.min.js"></script>
-    <script type="text/javascript" src="allReservations.js"></script>
-
-     <!-- jQuery読み込み -->
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
-    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
-
-  </head>
-
+  <link href="css/bootstrap.min.css" rel="stylesheet">
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+  <script src="js/bootstrap.min.js"></script>
+</head>
   <body>
-    <!--header部分-->
-    <img src="logo.jpg" width="125" height="70" align="top">
-    <div align="right"><a href="manager.php">管理者ホームへ</a></div>
-    <br>
-    <div class="h1">
-      <div class="container-fluid">
-        <div class="row">
-          <div class="col-sm-12" style="background-color:blue;"><center><font color="white">予約一覧</font></center>
-          </div>
-        </div>
+    <body>
+      <img src="logo.jpg" width="125" height="70" align="top">
+      <div align="right"><a href="manager.php">管理者ホームへ</a></div>
+      <div class="h1">
+        <center>
+          <font color="white">予約一覧</font>
+        </center>
       </div>
-    </div>
+      <br>
 <!--
   <center><font size="5"><div id="table" style="border: solid"></div></font></center>
 -->
