1d2870670db4ef0ed4c422bbf920b921ed669fba
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-11 19:21:34 +0900

    fix footer.css and add stub of getHomeInfo.js

---
+++ htdocs/stub/getHomeInfo.php
@@ -0,0 +1,38 @@
+{
+    "shopinfo":[
+        {
+            "name": "hoge",
+            "color": "000000",
+            "phonenumber": "00000000000",
+            "address": "Kochi",
+            "businessday": "月木",
+            "open": "00:00:00",
+            "close": "23:59:59"
+        },
+        {
+            "name": "hoge",
+            "color": "000000",
+            "phonenumber": "00000000000",
+            "address": "Kochi",
+            "businessday": "月木",
+            "open": "00:00:00",
+            "close": "23:59:59"
+        }
+    ],
+    "event":[
+        {
+            "storename": "hoge",
+            "isopen": 1,
+            "date": "2019-01-01",
+            "open": "00:00:00",
+            "close": "23:59:59"
+        },
+        {
+            "storename": "hoge",
+            "isopen": 1,
+            "date": "2019-01-01",
+            "open": "00:00:00",
+            "close": "23:59:59"
+        }
+    ]
+}
