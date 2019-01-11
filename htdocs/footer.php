585df155b5058ef1d0a2c5923b4afe0d74d2aace
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-11 19:07:31 +0900

    update header and footer

--- htdocs/footer.php
+++ htdocs/footer.php
@@ -1,5 +1,12 @@
+<link href="css/footer.css" rel="stylesheet">
+<footer class="footer mt-auto py-3">
+    <div class="container text-center">
+        <p class="text-muted" id="footer-text">MPARS</p>
+    </div>
+</footer>
 <!-- bootstrap，popperのjsファイル読み込み -->
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+</main>
 </body>
 </html>
