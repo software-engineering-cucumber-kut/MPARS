620ae221fabc6eb94d3dad26e9d87473e22236e0
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-11 18:01:22 +0900

    change putProduct.php to update database if items already exist

--- htdocs/putProduct.php
+++ htdocs/putProduct.php
@@ -6,14 +6,26 @@ if (isset($_SESSION['id'])) {
     global $last_insert_id;
     $json_string = file_get_contents('php://input');
     $json_obj = json_decode($json_string);
-    $target_string = 'INTO item (id, name, photo, description, price, storeid) VALUES (NULL, '.
+    $itemId = database('SELECT', 'id from item WHERE name = \'' . $json_obj->name . '\'');
+    $itemId = (int)$itemId[0]["id"];
+    $target_string = 'INTO item (id, name, photo, description, price, storeid) VALUES (' .
+        $itemId . ', ' .
         "'" . $json_obj->name . "'" . ',' .
         "'" . $json_obj->photo . "'" . ',' .
         "'" . $json_obj->description . "'" . ',' .
         $json_obj->price . ',' .
-        $json_obj->storeid . ')';
-    database('INSERT', $target_string);
-    echo "1";
+        $json_obj->storeid . ') ON DUPLICATE KEY UPDATE ' .
+        "name = '" . $json_obj->name . "', " .
+        "photo = '" . $json_obj->photo . "', " .
+        "description = '" . $json_obj->description . "', " .
+        "price = " . $json_obj->price . ", " .
+        "storeid = " . $json_obj->storeid;
+    $result = database('INSERT', $target_string);
+    if ($result) {
+        echo "1";
+    } else {
+        echo "0";
+    }
 } else {
     echo "0";
 }
