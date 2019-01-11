b8cad2ade1b13685078f1153873fdd1bef2e03ae
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-11 19:09:05 +0900

    add home.php and getHomeInfo.js

---
+++ htdocs/js/getHomeInfo.js
@@ -0,0 +1,45 @@
+// JavaScript Document
+const getHomeInfo = () => {
+    $.ajax({
+        type: 'GET',
+        url: 'http://localhost:8888/stub/getHomeInfo.php',
+        datatype: 'json'
+    }).done((res) => {
+//        console.log(res);
+        addHomeInfo(JSON.parse(res));
+    });
+};
+
+const addHomeInfo = (data) => {
+    let events = data.event;
+    let shopinfos = data.shopinfo;
+    
+    // イベント情報をホーム画面に追加
+    $.each(events, (index, val) => {
+        // 追加する文字列
+        var card = '<div class="card"><div class="card-header">臨時' +
+            (val.isopen ? '開' : '閉') +
+            '店のお知らせ </div> <ul class ="list-group list-group-flush"><li class="list-group-item"><strong>NAME : </strong>' +
+            val.storename +
+            '</li> <li class="list-group-item"><strong>DATE : </strong>' +
+            val.date +
+            '</li> <li class="list-group-item"><strong>HOURS : </strong>' +
+            val.open.match(/..:../) + '-' + val.close.match(/..:../) +
+            '</li></ul></div><br/>';
+        
+        $('#event-info-body').append(card);
+    });
+    
+    $.each(shopinfos, (index, val) => {
+        var card = '<div class="card"> <div class="card-header">' +
+            val.name +
+            '</div><ul class="list-group list-group-flush"><li class="list-group-item"><strong>ADDRESS : </strong>' + 
+            val.address +
+            '</li><li class="list-group-item"><strong>PHONE : </strong>' +
+            val.phonenumber +
+            '</li><li class="list-group-item"><strong>BUSINESS DAY : </strong>' +
+            val.businessday + ' ' + val.open.match(/..:../) + '-' + val.close.match(/..:../) +
+            '</li></ul></div><br/>';
+        $('#contact-info-body').append(card);
+    });
+};
