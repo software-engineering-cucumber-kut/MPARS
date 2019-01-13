c89b56840fd5836e5cdfab05e5bdbf044b8358b4
YamaD1 <32477483+YamaD1@users.noreply.github.com>
2019-01-14 08:08:56 +0900

    add getCompReservation.php

---
+++ htdocs/getCompReservation.php
@@ -0,0 +1,26 @@
+{
+	"reserveProducts":[
+<?php
+require('database.php');
+$reserveProducts = database('SELECT', '* FROM reservation JOIN reservationitems ON reservationid = reservation.id');
+// DBアクセス結果をforeachで取り出す
+foreach($reserveProducts as $val) {
+  $itemdata = database('SELECT', '* FROM item WHERE id =' . $val["id"]);
+    $itemdata = $itemdata[0];
+    
+    echo"\t\t{\n",
+        "\t\t\t" . '"received":' . $val["received"] . ",\n",
+        "\t\t\t" . '"reseavationtime":' . $val["reseavationtime"] . ",\n",
+         "\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
+        "\t\t\t" . '"itemname":' . $itemdatal["name"] . ",\n",
+        "\t\t\t" . '"amount":' . $val["amount"] . ",\n",
+          "\t\t\t" . '"price":' . $itemdata["price"] . ",\n";
+    if ($val == end($reserveProducts)) {
+        print("\t\t}\n");
+    } else {
+        print("\t\t},\n");
+    }
+}
+?>
+	]
+}
