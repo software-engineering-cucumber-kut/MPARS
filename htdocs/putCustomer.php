45ad6907a55da7e377dba9efb695b49ed91ab8dc
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-14 16:42:36 +0900

    Fix distinguish user type

--- htdocs/putCustomer.php
+++ htdocs/putCustomer.php
@@ -1,30 +1,46 @@
 {
-  "success":<?php
-  session_start();
-  if (isset($_SESSION['id'])) {
+  "success":
+  <?php
+  //session_start();
+  //if (isset($_SESSION['id'])) {
     require('database.php');
     // global $last_insert_id;
     $json_string = file_get_contents('php://input');
     $json_obj = json_decode($json_string);
 
     // 学生と一般を分ける
+    /*
     $student = $json_obj->studentuser;
     $general = $json_obj->generaluser;
-
-    if ($student != NULL) {
+    */
+    $typeOfUser = $json_obj->type;
+    //if ($student != NULL) {
+    if ($typeOfUser == 1)
       // 学生アカウント
-      foreach ($student as $studentuser) {
-        $target_string = 'INTO studentuser (id, name, phonenumber, password) VALUES (' . $json_obj->id . ',' . "'" . $json_obj->name . "'" . ',' . "'" . $json_obj->phonenumber . "'" . ',' . "'" . $json_obj->password . "'". ')';
-        database('INSERT', $target_string);
-      }
+      $target_string = 'INTO studentuser (id, name, phonenumber, password) VALUES (' .
+      $json_obj->id . ',' .
+      "'"$json_obj->name . "'" . ',' .
+      $json_obj->phonenumber . ',' .
+      "'" . $json_obj->password . "');";
+      $dbaccess_result = database('INSERT', $target_string);
     } else {
       // 一般アカウント
-      foreach ($generaluser as $generaluser) {
-        $target_string = 'INTO generaluser (id, name, phonenumber, password, address, birthday, mail) VALUES (' . $json_obj->id . ',' . "'" . $json_obj->name . "'" . ',' . "'" . $json_obj->phonenumber . "'" . ',' . "'" . $json_obj->password . "'". ',' . "'" . $json_obj->address . "'" . ',' . "'" . $json_obj->birthday . "'" . ',' . "'" . $json_obj->mail . "'"
-        ')';
-        database('INSERT', $target_string);
-      }
+      $target_string = 'INTO generaluser (id, name, phonenumber, password, address, birthday, mail) VALUES (' .
+      $json_obj->id . ',' .
+      "'" . $json_obj->name . "'" . ',' .
+      $json_obj->phonenumber . ',' .
+      "'" . $json_obj->password . "'". ',' .
+      "'" . $json_obj->address . "'" . ',' .
+      "'" . $json_obj->birthday . "'" . ',' .
+      "'" . $json_obj->mail ."');";
+      $dbaccess_result = database('INSERT', $target_string);
     }
+  //}
+  // Test
+  if ($dbaccess_result) {
+    echo "1";
+  } else {
+    echo "0";
   }
  ?>
 }
