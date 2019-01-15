638badc3b4a279c318fc724903db35503876cd4a
YamaD1 <32477483+YamaD1@users.noreply.github.com>
2019-01-15 10:51:00 +0900

    fix getCompReservation.php

--- htdocs/getCompReservation.php
+++ htdocs/getCompReservation.php
@@ -12,7 +12,7 @@ foreach($reserveProducts as $val) {
         "\t\t\t" . '"received":' . $val["received"] . ",\n",
         "\t\t\t" . '"reseavationtime":' . $val["reseavationtime"] . ",\n",
          "\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
-        "\t\t\t" . '"itemname":' . $itemdatal["name"] . ",\n",
+        "\t\t\t" . '"itemname":' . $itemdata["name"] . ",\n",
         "\t\t\t" . '"amount":' . $val["amount"] . ",\n",
           "\t\t\t" . '"price":' . $itemdata["price"] . ",\n";
     if ($val == end($reserveProducts)) {
