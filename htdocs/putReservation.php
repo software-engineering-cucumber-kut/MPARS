9119cabf84e5490fbbfd3326875ac0fa09a11157
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-16 19:15:33 +0900

    Fix putting reservation data method

--- htdocs/putReservation.php
+++ htdocs/putReservation.php
@@ -5,10 +5,17 @@
     require('database.php');
     $json_string = file_get_contents('php://input');
     $json_obj = json_decode($json_string);
-    $target_string = 'INTO reservation (id, userid, recieved, reservationtime, receiveday) VALUES (NULL, ' .  "'" . $json_obj->userid . "'" . ',' . "'" . $json_obj->received . "'" . ',' . "'" . $json_obj->reservationtime . "'" . ',' . "'" . $json_obj->receiveday . ')';
-    database('INSERT', $target_string);
-    echo "1";
-    echo "0";
+    $target_string = 'INTO reservation
+    (userid, recieved, receiveday)
+    VALUES (' .
+    $json_obj->userid . ',' .
+    $json_obj->received . ',' . "'" .
+    $json_obj->receiveday . "')";
+    $dbaccess_result = database('INSERT', $target_string);
+    if ($dbaccess_result) {
+      echo "1";
+    } else {
+      echo "0";
     }
   }
  ?>
