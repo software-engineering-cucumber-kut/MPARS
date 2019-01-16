b3e45d850ce7885e01db576316ea6ab9a8cfc44a
mikarinn <200326q@ugs.kochi-tech.ac.jp>
2019-01-16 23:25:20 +0900

    update sendProduct.js

--- sendProduct.js
+++ sendProduct.js
@@ -22,33 +22,53 @@ function previewFile() {
     }
 }
 
-// 保存ボタンを押したとき入力したデータの保存
-function Saveproduct() {
-    // 画像ファイルの取得
-    var myimg = document.getElementById('photo');
-    var photo = myimg.src;
-    // 商品名の取得
-    var name = document.getElementById("name").value;
-    // 値段の取得
-    var price = document.getElementById("price").value;
-    // 商品説明文の取得
-    var description = document.getElementById("description").value;
-
-        // localstorageにJSON形式で保存する
-        var datalist = {
-            data1: photo,
-            data2: name,
-            data3: price,
-            data4: description
-        }
-        localStorage.setItem('config', JSON.stringify(datalist));     
-}
-
-// localstorageに保存したデータをAjaxでPHPに送信する
-
-
 // 削除ボタンを押したとき選択した商品情報の削除
 function Deleteproduct() {
-    
+    window.location.href = "editProducts.php"; // ページ遷移
 }
 
+
+// 保存ボタンを押したとき入力したデータの保存
+function Sendproduct() {
+    const sendProduct = () => {
+        let product_json = {
+            contents: $('#name').val()
+        };
+        let send_product = JSON.stringify(product_json);
+
+        // 画像ファイルの取得
+        /*var myimg = document.getElementById('photo');
+        var photo = myimg.src;*/
+        // 商品名の取得
+        //var name = document.getElementById("name").value;
+        // 値段の取得
+        //var price = document.getElementById("price").value;
+        // 商品説明文の取得
+        //var description = document.getElementById("description").value;
+        // 送信するオブジェクトの作成
+        /*var obj = {
+            name: name,
+            price: price,
+            description: description
+        }
+        // オブジェクトをJSON型にする
+        var $send_data = JSON.stringify(obj);
+        console.log($send_data);*/
+
+
+        // ajax通信
+        $.ajax({
+            type: 'POST',
+            url: 'http://localhost:8888/stub/putProduct.php',
+            datatype: 'json',
+            data: send_product
+        }).done((res) => {
+            if (JSON.parse(res).success) {
+                location.href = "http://localhost:8888/editProducts.php";
+                console.log(send_product);
+            } else {
+                console.log("product sending failed");
+            }
+        });
+    };
+}
