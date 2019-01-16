e8b1b240e339185918e664262c4517285626ea28
YamaD1 <32477483+YamaD1@users.noreply.github.com>
2019-01-16 17:57:18 +0900

    fix sql

--- htdocs/getCompReservation.php
+++ htdocs/getCompReservation.php
@@ -2,7 +2,8 @@
 	"reserveProducts":[
 <?php
 require('database.php');
-$reserveProducts = database('SELECT', '* FROM reservation JOIN reservationitems ON reservationid = reservation.id');
+$reserveProducts = database('SELECT', '* FROM reservation JOIN reservationitems ON reservationitems.reservationid = reservation.id');
+//$reserveProducts = database('SELECT', '* FROM reservation);
 // DBアクセス結果をforeachで取り出す
 foreach($reserveProducts as $val) {
   $itemdata = database('SELECT', '* FROM item WHERE id =' . $val["id"]);
@@ -10,17 +11,22 @@ foreach($reserveProducts as $val) {
     
     echo "\t\t{\n",
          "\t\t\t" . '"received":' . $val["received"] . ",\n",
-         "\t\t\t" . '"reseavationtime":' . $val["reseavationtime"] . ",\n",
+         "\t\t\t" . '"reservationtime":' . $val["reservationtime"] . ",\n",
          "\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
          "\t\t\t" . '"itemname":' . $itemdata["name"] . ",\n",
          "\t\t\t" . '"amount":' . $val["amount"] . ",\n",
-         "\t\t\t" . '"price":' . $itemdata["price"] . ",\n";
+         "\t\t\t" . '"price":' . $itemdata["price"] . "\n";
     if ($val == end($reserveProducts)) {
         print("\t\t}\n");
     } else {
         print("\t\t},\n");
     }
+    
+   // print_r($itemdata);
 }
+
+//print_r($reserveProducts);
+
 ?>
 	]
 }
