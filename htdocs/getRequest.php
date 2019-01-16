beb4aae41d859edf74102297108b8b58467f939e
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-16 17:14:07 +0900

    Update edit indents

--- htdocs/getRequest.php
+++ htdocs/getRequest.php
@@ -5,15 +5,15 @@ require('database.php');
 $request = database('SELECT', '* FROM request');
 //配列requestから値(contents,date)を順に取り出し$valに格納
 foreach($request as $val) {
- echo   "\t\t{\n",
-        "\t\t\t" . '"contents":' . $val["contents"] . ",\n",
-        "\t\t\t" . '"datetime":' . $val["datetime"] . "\n";
-        //最後まで行ったときの処理
-    if ($val == end($request)) {
-        print("\t\t}\n");
-    } else {
-        print("\t\t},\n");
-    }
+  echo   "\t\t{\n",
+         "\t\t\t" . '"contents":' . $val["contents"] . ",\n",
+         "\t\t\t" . '"datetime":' . $val["datetime"] . "\n";
+         //最後まで行ったときの処理
+  if ($val == end($request)) {
+    print("\t\t}\n");
+  } else {
+    print("\t\t},\n");
+  }
 }
 ?>
     ]
