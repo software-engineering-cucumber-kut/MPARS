b262f4284d6c00a03529eb07005c02f370a377fa
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-18 14:43:13 +0900

    Fix show requests list

--- showRequests.php
+++ showRequests.php
@@ -6,14 +6,14 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <!--レスポンシブwebデザインに必要なmetaタグ-->
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Bootstrap Sample</title>
+    <title>Mpars</title>
     <!--BootstrapのJavascriptよりも先にJQueryを読み込むようにしないとBootstrapのjavascriptがうまく動作しない-->
     <!-- BootstrapのCSS読み込み -->
-    <link href="css/bootstrap.min.css" rel="stylesheet">
+    <!--<link href="css/bootstrap.min.css" rel="stylesheet">-->
     <!-- jQuery読み込み -->
     <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
     <!-- BootstrapのJS読み込み -->
-    <script src="js/bootstrap.min.js"></script>
+    <!--<script src="/js/bootstrap.min.js"></script>-->
 </head>
 <body>
 <div class="container-fluid">
@@ -21,18 +21,18 @@
     <!--グリッドシステムではcol-lg(md,sm,xs)は12段階で画面を分割できる-->
         <div class="col-sm-2">
         <div class="d-flex">
-        <div class="header-img"><img src="logo.jpg" width = "120" ></div>
+        <div class="header-img"><img src="/img/logo.svg" width = "120" ></div>
         </div>
         </div>
 
         <div class="col-sm-8"></div>
         <div align = "right" class="col-sm-2">
         <a href="manage.php" style="font-size:30px;" onclick="location.href='./manager.php'">管理者メニューへ</a>
-        </div> 
-        <!--<input type="button" class="managehome_btn" value="管理者ホームへ" style="font-size:30px; color:blue;" id="button1">--> 
+        </div>
+        <!--<input type="button" class="managehome_btn" value="管理者ホームへ" style="font-size:30px; color:blue;" id="button1">-->
     </div>
     <br>
-    
+
 
     <div class="row">
     <div align = "center" class="col-sm-12" style="background-color:#8EB8FF">
@@ -41,9 +41,8 @@
     </div>
 
     <br>
-    <div>
-        2018/10/8/ 12:30 <br>
-        エビを使ったパンが欲しいです。<br>
+    <div id="requests-body">
+
     </div>
     <br>
     <div class = "row">
@@ -52,18 +51,11 @@
     </div>
 
     <br>
-    <div>
-        2018/10/13/ 17:30<br>
-        お金が欲しいです。万年金欠でパンも買えません。<br>
-    </div>
-
-    <br>
-    <div>
-        2018/11/10/ 5:30<br>
-        ピザみたいなパンが欲しいです。お菓子の種類をもっと増やして欲しいです。<br>
-    </div>
-    <br>
 
 </div>
+<script type="text/javascript" src="js/getRequests.js"></script>
+<script type="text/javascript">
+    getRequests();
+</script>
 </body>
 </html>
