56858620957455c901664fa3b2093c85fc0adeec
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-21 02:35:04 +0900

    Fix send json

--- htdocs/getRequest.php
+++ htdocs/getRequest.php
@@ -6,8 +6,8 @@ $request = database('SELECT', '* FROM request');
 //配列requestから値(contents,date)を順に取り出し$valに格納
 foreach($request as $val) {
   echo   "\t\t{\n",
-         "\t\t\t" . '"contents":' . $val["contents"] . ",\n",
-         "\t\t\t" . '"datetime":' . $val["datetime"] . "\n";
+         "\t\t\t" . '"contents":"' . $val["contents"] . '"' . ",\n",
+         "\t\t\t" . '"datetime":"' . $val["datetime"] . '"' . "\n";
          //最後まで行ったときの処理
   if ($val == end($request)) {
     print("\t\t}\n");
