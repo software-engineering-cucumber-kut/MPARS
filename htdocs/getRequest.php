a518cf2db5c562cc82ca6fba44f61260ad6dac55
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-21 05:11:55 +0900

    fixed

--- htdocs/getRequest.php
+++ htdocs/getRequest.php
@@ -2,10 +2,11 @@
     "request": [
 <?php
 require('database.php');
-$request = database('SELECT', '* FROM request');
+$request = database('SELECT', ' * FROM request');
 //配列requestから値(contents,date)を順に取り出し$valに格納
 foreach($request as $val) {
   echo   "\t\t{\n",
+         "\t\t\t" . '"id":' . $val["id"] . ",\n",
          "\t\t\t" . '"contents":"' . $val["contents"] . '"' . ",\n",
          "\t\t\t" . '"datetime":"' . $val["datetime"] . '"' . "\n";
          //最後まで行ったときの処理
