326cd35188689cbec10ed13195e0d75c88d1a06c
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-21 05:10:56 +0900

    fix json

--- htdocs/getCompReservation.php
+++ htdocs/getCompReservation.php
@@ -8,12 +8,12 @@ $reserveProducts = database('SELECT', '* FROM reservation JOIN reservationitems 
 foreach($reserveProducts as $val) {
   $itemdata = database('SELECT', '* FROM item WHERE id =' . $val["id"]);
   $itemdata = $itemdata[0];
-    
+
     echo "\t\t{\n",
          "\t\t\t" . '"received":' . $val["received"] . ",\n",
-         "\t\t\t" . '"reservationtime":' . $val["reservationtime"] . ",\n",
+         "\t\t\t" . '"reservationtime":"' . $val["reservationtime"] . '"' . ",\n",
          "\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
-         "\t\t\t" . '"itemname":' . $itemdata["name"] . ",\n",
+         "\t\t\t" . '"itemname":"' . $itemdata["name"] . '"' . ",\n",
          "\t\t\t" . '"amount":' . $val["amount"] . ",\n",
          "\t\t\t" . '"price":' . $itemdata["price"] . "\n";
     if ($val == end($reserveProducts)) {
@@ -21,7 +21,7 @@ foreach($reserveProducts as $val) {
     } else {
         print("\t\t},\n");
     }
-    
+
    // print_r($itemdata);
 }
 
