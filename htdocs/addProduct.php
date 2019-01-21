fabc3fe8f8119c73153f5272b8e753eb8dd4b1a8
mikarinn <200326q@ugs.kochi-tech.ac.jp>
2019-01-22 01:00:11 +0900

    update addProduct.php

--- htdocs/addProduct.php
+++ htdocs/addProduct.php
@@ -7,7 +7,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>MPARS</title>
         <script type="text/javascript" src="a_sendProduct.js"></script>
-        <!--<script type="text/javascript" src="getProducts.js"></script>-->
         <link rel="stylesheet" type="text/css" href="menu.css">
         <!-- BootstrapのCSS読み込み -->
         <link href="css/bootstrap.min.css" rel="stylesheet">
@@ -16,48 +15,52 @@
         <!-- BootstrapのJS読み込み -->
         <script src="js/bootstrap.min.js"></script>
     </head>
-
+    
 <body>
     <!-- Start your code here -->
 
     <!--header部分-->
     <img src="logo.jpg" width="125" height="70" align="top">
-    <div align="right"><a href="manager.php">管理者ホームへ</a></div>
+    <div align="right"><a href="manager.php" style="font-size:20px;">管理者ホームへ</a></div>
     <div class="h1">
         <center><font color="white">メニュー編集</font></center>
     </div>
     <br>
     
         <!-- 商品追加のフォーム -->
-    <form action="editProducts.php" method="post" name="info">
+    <form method="post" name="info">
         <!-- 商品画像の選択 -->
         <input type="file" id="select_photo" accept="image/*" onchange="previewFile()" required>
         <br>
         <br>
         <!-- 初期画像 -->
         <img id="photo" src="noimage.jpg" height="100" width="100">
- 
        
-        <p>
-            <label>商品名:</label><br>
+        
+        <b><p><label>商品名:</label>
+        <br>
             <input type="text" id="name" placeholder="メロンパン" maxlength="20" required><!--required(入力必須)-->
-        </p>
-        <p>
-            <label>値段:</label><br>
+            </p></b>
+        
+        <b><p><label>値段:</label>
+            <br>
             <input type="number" id="price" placeholder="250" required>
             ￥
-        </p>
-        <p>
-            <label>商品説明文:</label><br>
-            <textarea id="description" placeholder="テキストを入力して下さい。(500文字以内)" maxlength="500" cols="50" rows="4" required></textarea>
-        </p>
-        <p>
-            <input type="submit" value="保存" class="btn btn-primary" onclick="Sendproduct()" />
-        </p>
+            </p></b>
         
+        <b><p><label>商品説明文:</label>
+            <br>
+            <textarea id="description" placeholder="テキストを入力して下さい。(500文字以内)" maxlength="500" cols="50" rows="4" required></textarea>
+            </p></b>
+        
+        <p>
+            <input type="button" value="保存" class="btn btn-primary" onclick="sendProduct()" />
+        </p>
     </form>
-    
     <!-- End your code here -->
 </body>
 
 </html>
+
+<!--<div id="b"></div>
+<script type="text/javascript" src="sendProduct.js?"></script>-->
