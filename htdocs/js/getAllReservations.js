8d5c7b0f96d4a26fae9f680e886ab585dbf3b62f
tatsuki sato <imperially9@gmail.com>
2019-01-21 23:52:19 +0900

    update getAllReservations.js

--- htdocs/js/getAllReservations.js
+++ htdocs/js/getAllReservations.js
@@ -16,21 +16,23 @@ const addAllReservation = (data) => {
     // イベント情報をホーム画面に追加
     $.each(reservations, (index, val) => {
         // 追加する文字列
-        var card = val.reservationtime + '  ' +
-            val.receiveday + '  ' +
-            val.amount + '  ' +
-            val.reservationid + '  ' +
+        var card = val.reservationtime + '        ' +
+            val.receiveday + '        ' +
+            val.amount + '        ' +
+            val.reservationid + '        ' +
             '<input type="button" value="詳細情報" onclick="clickBtn2()" /><div id="p2">' +
-            val.phonenumber + '  ' +
-            val.mail + '  ' +
-            val.address + '  ' +
+            val.phonenumber + '        ' +
+            val.mail + '        ' +
+            val.address + '        ' +
             '</div>';
 
         $('#AllReservations').append(card);
     });
 };
 
-document.getElementById("p2").style.visibility = "hidden";
+window.onload = function(){
+  document.getElementById("p2").style.visibility = "hidden";
+}
 
 function clickBtn2(){
 	const p2 = document.getElementById("p2");
@@ -45,32 +47,32 @@ function clickBtn2(){
 }
 
 // 表の動的作成
-function makeTable(data, tableId){
+//function makeTable(data, tableId){
     // 表の作成開始
-    var rows=[];
-    var table = document.createElement("table");
+  //  var rows=[];
+    //var table = document.createElement("table");
 
     // 表に2次元配列の要素を格納
-    for(i = 0; i < data.length; i++){
-        rows.push(table.insertRow(-1));  // 行の追加
-        for(j = 0; j < data[0].length; j++){
-            cell=rows[i].insertCell(-1);
-            cell.appendChild(document.createTextNode(data[i][j]));
+    //for(i = 0; i < data.length; i++){
+      //  rows.push(table.insertRow(-1));  // 行の追加
+        //for(j = 0; j < data[0].length; j++){
+          //  cell=rows[i].insertCell(-1);
+            //cell.appendChild(document.createTextNode(data[i][j]));
             // 背景色の設定
-            if(i==0){
-                cell.style.backgroundColor = "orange"; // ヘッダ行
-            }else{
-                cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
-            }
-        }
-    }
+            //if(i==0){
+            //    cell.style.backgroundColor = "orange"; // ヘッダ行
+            //}else{
+            //    cell.style.backgroundColor = "#ddd"; // ヘッダ行以外
+            //}
+        //}
+  //  }
     // 指定したdiv要素に表を加える
-    document.getElementById(tableId).appendChild(table);
-}
-window.onload = function(){
+  //  document.getElementById(tableId).appendChild(table);
+//}
+//window.onload = function(){
 // 表のデータ
-var data = [["予約日", "受取日時", "予約内容", "学籍番号またはID", "詳細情報"]];
+//var data = [["予約日", "受取日時", "予約内容", "学籍番号またはID", "詳細情報"]];
 
 // 表の動的作成
-makeTable(data,"table");
-};
+//makeTable(data,"table");
+//};
