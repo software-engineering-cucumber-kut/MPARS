af76444a730c1b6879ad6d954b52048d5896ecc4
tatsuki sato <imperially9@gmail.com>
2019-01-21 23:50:51 +0900

    update allReservations.php

--- htdocs/allReservations.php
+++ htdocs/allReservations.php
@@ -7,17 +7,16 @@
   <meta name="viewport" content="width=device-width, initial-scale=1">
   <title>MPARS</title>
   <!--jsとcssの読み込み-->
-  <script type="text/javascript" src="a_allReservations.js"></script>
+  <!--<script type="text/javascript" src="js/a_allReservations.js"></script>-->
 
-
-  <link rel="stylesheet" type="text/css" href="allReservations.css">
+  <link rel="stylesheet" type="text/css" href="css/allReservations.css">
 
   <link href="css/bootstrap.min.css" rel="stylesheet">
   <script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
   <script src="js/bootstrap.min.js"></script>
 </head>
   <body>
-      <img src="logo.jpg" width="125" height="70" align="top">
+      <img src="img/logo.jpg" width="125" height="70" align="top">
       <div align="right"><a href="manager.php">管理者ホームへ</a></div>
       <div class="h1">
         <center>
@@ -27,11 +26,20 @@
       <br>
       <br>
       <center>
-      <div id ="table"></div>
+      <!--<div id ="table"></div>-->
+      <table>
+        <tr>
+          <th>予約日</th>
+          <th>受取日時</th>
+          <th>予約内容</th>
+          <th>学籍番号またはID</th>
+          <th>詳細情報</th>
+        </tr>
+      </table>
       <div id ="AllReservations"></div>
       </center>
 
-      <script type="text/javascript" src="getAllReservations.js"></script>
+      <script type="text/javascript" src="js/getAllReservations.js"></script>
       <script type="text/javascript">
         getAllReservation();
       </script>
