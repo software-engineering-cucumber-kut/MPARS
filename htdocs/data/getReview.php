9491bb61daafdec88615cfb7fb1e8f56683a6112
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-23 16:24:04 +0900

    change specification of getReview.php

--- htdocs/data/getReview.php
+++ htdocs/data/getReview.php
@@ -6,24 +6,22 @@
   // foreach($review as $val) {
     $json_string = file_get_contents('php://input');
     $json_obj = json_decode($json_string);
-    // 評価の平均値を取得
-    $target = ' AVG(evaluation) FROM review GROUP BY itemid';
-    // echo $target . "\n";
-    $evaluation_obj = database('SELECT', $target);
-    // $evaluation_avg = intval($evaluation_obj[0]["AVG(evaluation)"]);
-    //foreach($request as $val) {
-    for ($i = 0; $i < count($evaluation_obj); $i++) {
-      $evaluation_avg = intval($evaluation_obj[$i]["AVG(evaluation)"]);
+
+    $target = ' * FROM review WHERE itemid = ' . strval($json_obj->itemid);
+    $reviews = database('SELECT', $target);
+
+    for ($i = 0; $i < count($reviews); $i++) {
       echo"\t\t{\n",
           //"\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
-          "\t\t\t" . '"evaluation":' . $evaluation_avg . "\n";
+          "\t\t\t" . '"evaluation":' . $reviews[$i]["evaluation"] . ",\n" .
+          "\t\t\t" . '"contents":' . $reviews[$i]["contents"] . ",\n" .
+          "\t\t\t" . '"datetime":' . $reviews[$i]["datetime"] . "\n";
     	/*"\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
-      "\t\t\t" . '"contents":' . $val["contents"] . ",\n",
       "\t\t\t" . '"replyid":' . $val["replyid"] . ",\n",
       "\t\t\t" . '"evaluation":' . $evaluation_avg . ",\n",
       "\t\t\t" . '"datetime":' . $val["datetime"] . "\n";*/
       //最後まで行ったときの処理
-      if ($val == end($evaluation_obj)) {
+      if ($i >= count($reviews)-1) {
         print("\t\t}\n");
       } else {
         print("\t\t},\n");
