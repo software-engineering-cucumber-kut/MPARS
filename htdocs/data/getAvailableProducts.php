5e7a3fab6186676d52c3979b13675538d50a3963
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-24 05:26:49 +0900

    Fix get json data

--- htdocs/data/getAvailableProducts.php
+++ htdocs/data/getAvailableProducts.php
@@ -1,44 +1,39 @@
-<?php
-  require('database.php');
-  $product = $_GET[itemid];
-  $stock = $_GET[stock];
+{
+  "Products": [
+  <?php
+    require('database.php');
 
-  foreach($stock as $val) {
-    // 在庫数取得
-    $target = ' currentstock FROM stock WHERE itemid = ' . $val["id"];
+    $target = ' item.id,
+                item.name,
+                item.photo,
+                item.description,
+                item.price,
+                stock.currentstock,
+                stock.nextstock
+                FROM item, stock
+                WHERE stock.itemid = item.id';
 
-    $evaluation_obj = database('SELECT', $target);
-    $evaluation = intval($evaluation_obj[0]["currentstock"]);
+    $products = database('SELECT', $target);
 
-    if ($stock > $evaluation) {
-      echo "\t\t\t" . '"itemid:"' . $val["itemid"] . ",\n",
-           "\t\t\t" . '"currentstock:"' . $val["stock"] . ",\n";
-      /*
-      if ($val == end($evaluation_obj)) {
+    foreach($products as $val) {
+      // 在庫数取得
+
+      echo  "\t\t{\n",
+            "\t\t\t" . '"id":' . $val["id"] . ",\n",
+            "\t\t\t" . '"name":"' . $val["name"] . '"' . ",\n",
+            "\t\t\t" . '"photo":"' . $val["photo"] . '"' . ",\n",
+            "\t\t\t" . '"description":"' . $val["description"] . '"' . ",\n",
+            "\t\t\t" . '"price":' . $val["price"] . ",\n",
+            "\t\t\t" . '"currentstock":' . $val["currentstock"] . ",\n",
+            "\t\t\t" . '"nextstock":' . $val["nextstock"] . "\n";
+
+      if ($val == end($products)) {
         print("\t\t}\n");
       } else {
         print("\t\t), \n");
       }
-      */
-      add();
-    } else {
-      echo "\t\t\t" . '"response:"' . '0' . ",\n";
-      /*
-      if ($val == end($evaluation_obj)) {
-        print("\t\t}\n");
-      } else {
-        print("\t\t), \n");
-      }
-      */
-      add();
     }
-  }
 
-  function add($val, $evaluation_obj) {
-    if ($val == end($evaluation_obj)) {
-      print("\t\t}\n");
-    } else {
-      print("\t\t), \n");
-    }
-  }
-?>
+  ?>
+  ]
+}
