69e93fbaf53e46ad401d2be6f3568403ee47c12b
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-24 15:43:29 +0900

    change getCompReservation.php to use session

--- htdocs/data/getCompReservation.php
+++ htdocs/data/getCompReservation.php
@@ -2,30 +2,33 @@
 	"reserveProducts":[
 <?php
 require('database.php');
-$reserveProducts = database('SELECT', '* FROM reservation JOIN reservationitems ON reservationitems.reservationid = reservation.id');
-//$reserveProducts = database('SELECT', '* FROM reservation);
-// DBアクセス結果をforeachで取り出す
-foreach($reserveProducts as $val) {
-  $itemdata = database('SELECT', '* FROM item WHERE id =' . $val["id"]);
-  $itemdata = $itemdata[0];
+session_start();
+if ($_SESSION["id"] != NULL) {
+    $reserveProducts = database('SELECT', '* FROM reservation JOIN reservationitems ON reservationitems.reservationid = reservation.id WHERE reservation.userid = ' . $_SESSION["id"]);
+    //$reserveProducts = database('SELECT', '* FROM reservation);
+    // DBアクセス結果をforeachで取り出す
+    foreach($reserveProducts as $val) {
+        $itemdata = database('SELECT', '* FROM item WHERE id =' . $val["itemid"]);
+        $itemdata = $itemdata[0];
 
-    echo "\t\t{\n",
-         "\t\t\t" . '"received":' . $val["received"] . ",\n",
-         "\t\t\t" . '"reservationtime":"' . $val["reservationtime"] . '"' . ",\n",
-         "\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
-         "\t\t\t" . '"itemname":"' . $itemdata["name"] . '"' . ",\n",
-         "\t\t\t" . '"amount":' . $val["amount"] . ",\n",
-         "\t\t\t" . '"price":' . $itemdata["price"] . "\n";
-    if ($val == end($reserveProducts)) {
-        print("\t\t}\n");
-    } else {
-        print("\t\t},\n");
+        echo "\t\t{\n",
+            "\t\t\t" . '"received":' . $val["received"] . ",\n",
+            "\t\t\t" . '"reservationtime":"' . $val["reservationtime"] . '"' . ",\n",
+            "\t\t\t" . '"itemid":' . $val["itemid"] . ",\n",
+            "\t\t\t" . '"itemname":"' . preg_split("/===/", $itemdata["description"])[0] . '"' . ",\n",
+            "\t\t\t" . '"amount":' . $val["amount"] . ",\n",
+            "\t\t\t" . '"price":' . $itemdata["price"] . "\n";
+        if ($val == end($reserveProducts)) {
+            print("\t\t}\n");
+        } else {
+            print("\t\t},\n");
+        }
+
+        // print_r($itemdata);
     }
 
-   // print_r($itemdata);
+    //print_r($reserveProducts);
 }
-
-//print_r($reserveProducts);
 
 ?>
 	]
