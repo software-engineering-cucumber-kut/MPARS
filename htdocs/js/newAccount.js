8883dd1085a636566444558b6f90a6da61ab0b42
Chan <yonezawakyoichirou@gmail.com>
2019-01-24 17:18:01 +0900

    change color and fontsize

--- htdocs/js/newAccount.js
+++ htdocs/js/newAccount.js
@@ -1,45 +1,91 @@
+
+
 /*新規アカウント登録情報の送信 */
 
-let accountType = '';
 
-const newAccount = () => {
-    let sending_string = '';
-    if(accountType === 'student'){
-        let newAccount_json = { 
-            isstudent: 1,
-            id: $('#sUserid').val() ,
-            name: $('#sname').val() ,
-            phonenumber: $('#sphonenumber').val() ,
-            password: $('#password').val() 
-        };
-        sending_string = JSON.stringify(newAccount_json);
-    } else if(accountType === 'general') {
-        isstudent = 0;
-        let newAccount_json = { 
-            isstudent: 0,
-            name: $('#name').val() ,
-            phonenumber: $('#phonenumber').val() ,
-            password: $('#password2').val(), 
-            address: $('#address').val() ,
-            birthday: $('#birthday').val().replace(/(年|月|日)/g, '-') ,
-            mail: $('#mail').val()  
-        };
-        sending_string = JSON.stringify(newAccount_json);
+function student_checkForm(){
+    if(document.form1.input01.value == "" 
+    || document.form1.input02.value == ""
+    || document.form1.input03.value == ""
+    || document.form1.input04.value == ""){
+        alert("必須項目を全て入力して下さい。");
+	return false;
+    }else{
+    student_newAccount();  
     }
+}
+
+function general_checkForm(){
+    if(document.form2.input01.value == "" 
+    || document.form2.input02.value == ""
+    || document.form2.input03.value == ""
+    || document.form2.input04.value == ""
+    || document.form2.input05.value == ""
+    || document.form2.input06.value == ""){
+        alert("必須項目を全て入力して下さい。");
+	return false;
+    }else{
+    general_newAccount();
+    }
+}
+
+
+
+const student_newAccount = () => {
+    let newAccount_json = { 
+        isstudent: "1",
+        id: $('#sUserid').val(),
+        name: $('#sname').val() ,
+        phonenumber: $('#sphonenumber').val() ,
+        password: $('#password').val() 
+    }
+
+    let sending_string = JSON.stringify(newAccount_json);
+    $( '#result' ).html(sending_string);
+    $.ajax({
+        type: 'POST',
+        url: 'data/putCustomer.php',
+        datatype: 'json',
+        data: sending_string
+      }).done((res) => {
+          if (JSON.parse(res).success) {
+              location.href = "/loginCustomer.php";
+         } else {
+             console.log("newAccount sending failed");
+         }
+      });
+}
+
+
+
+const general_newAccount = () => {
+    let newAccount_json = { 
+        isstudent: "0",
+        id: "" ,
+        name: $('#dname').val() ,
+        phonenumber: $('#phonenumber').val() ,
+        password: $('#password2').val(), 
+        address: $('#address').val() ,
+        birthday: $('#birthday').val().replace(/(年|月|日)/g, '-') ,
+        mail: $('#mail').val() 
+    }
+
+    let sending_string = JSON.stringify(newAccount_json);
+    $( '#sresult' ).html( sending_string );
 
     $.ajax({
         type: 'POST',
-        url: 'http://localhost:8888/data/putCustomer.php',
+        url: 'data/putCustomer.php',
         datatype: 'json',
         data: sending_string
-    }).done((res) => {
-        if (JSON.parse(res).success) {
-            location.href = "http://localhost:8888/loginCustomer.php";
-        } else {
-            console.log("newAccount sending failed");
-        }
-    });
-};
+      }).done((res) => {
+          if (JSON.parse(res).success) {
+              location.href = "/loginCustomer.php";
+         } else {
+             console.log("newAccount sending failed");
+         }
+      });
+}
 
 
 /* newAccount.jsの送信データ
@@ -54,3 +100,8 @@ const newAccount = () => {
 	“mail”:
 }
 */
+
+
+
+
+
