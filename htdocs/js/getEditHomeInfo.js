f89ed434459f0a332ea13b36b30fb7da9731a8fd
200367n <200367n@ugs.kochi-tech.ac.jp>
2019-01-26 17:27:42 +0900

    新規ファイル:ホーム編集画面の情報取得する機能

---
+++ htdocs/js/getEditHomeInfo.js
@@ -0,0 +1,81 @@
+// JavaScript Document
+const getEditHomeInfo = () => {
+    $.ajax({
+        type: 'GET',
+        url: '/data/getHomeInfo.php',
+        datatype: 'json'
+    }).done((res) => {
+//        console.log(res);
+        addEditHomeInfo(JSON.parse(res));
+    });
+};
+
+const addEditHomeInfo = (data) => {
+    let events = data.event;
+    let shopinfos = data.shopinfo;
+    $.each(events, (index, val) => {
+      var eventCard = '店名<input type="text" name="name-event-info-textarea" size="20" maxlength="10" value="' + val.storename + '"></input><br>日付<input type="date" name="event-calendar" max="9999-12-31"><br>時間' + selectHours(val.open.match(/..:../)) + selectMinutes(val.open.match(/..:../)) + '-' + selectHours(val.close.match(/..:../)) + selectMinutes(val.close.match(/..:../)) + '<br><br>';
+      $('#event-info-body').append(eventCard);
+    });
+    $.each(shopinfos, (index, val) => {
+      var shopInfoCard = '店名<input type="text" name="name-contact-textarea" size="20" maxlength="10" value="' + val.name + '"></input><br>住所<input type="text" name="adress-contact-textarea" size="60" maxlength="30" value="' + val.address + '"></input><br>電話番号<input type="text" name="phone-contact-textarea" size="22" maxlength="11" value="' + val.phonenumber + '"></input><br>営業曜日<input type="text" name="day-contact-textarea" size="14" maxlength="7" value="' + val.businessday + '"></input><br>営業時間' + selectHours(val.open.match(/..:../)) + selectMinutes(val.open.match(/..:../)) + '-' + selectHours(val.close.match(/..:../)) + selectMinutes(val.close.match(/..:../)) + '<br><br>';
+      $('#contact-info-body').append(shopInfoCard);
+    });
+};
+
+const selectHours = (data) => {
+  data = data.slice(0,-6);
+  var hours = new Array();
+  var selectHour = 24;
+  var selectOption = '<select name="startTime-hours-event"><option value="' + data + '" selected>' + data + '</option>';
+  for (var i =0; i < selectHour; i++) {
+    hours[i] = i;
+    selectOption = selectOption + '<option value="' + hours[i] + '">' + hours[i] + '</option>';
+  }
+  selectOption = selectOption + '</select>時';
+  return selectOption;
+}
+
+const selectMinutes = (data) => {
+  data = data.slice(3);
+  data = data.slice(0,-3);
+  var minutes = new Array();
+  var selectMinute = 60;
+  var selectOption = '<select name="startTime-minutes-event"><option value="' + data + '" selected>' + data + '</option>';
+  for (var i = 0; i < selectMinute; i++) {
+    minutes[i] = i;
+    selectOption = selectOption + '<option value="' + minutes[i] + '">' + minutes[i] + '</option>';
+  }
+  selectOption = selectOption + '</select>分';
+  return selectOption;
+}
+
+/*const selectHours = () => {
+  let hours = new Array();
+  let selectHour = 24;
+  let selectOption = '<select name="startTime-hours-event"><option value="0" selected>0</option>';
+  for (let i =1; i < selectHour; i++) {
+    hours[i] = i;
+    selectOption = selectOption + '<option value="' + hours[i] + '">' + hours[i] + '</option>';
+  }
+  selectOption = selectOption + '</select>時';
+  $('#event-start-select-hour').append(selectOption);
+  $('#event-end-select-hour').append(selectOption);
+  $('#contact-start-select-hour').append(selectOption);
+  $('#contact-end-select-hour').append(selectOption);
+}
+
+const selectMinutes = () => {
+  let minutes = new Array();
+  let selectMinute = 60;
+  let selectOption = '<select name="startTime-minutes-event"><option value="0" selected>0</option>';
+  for (let i = 1; i < selectMinute; i++) {
+    minutes[i] = i;
+    selectOption = selectOption + '<option value="' + minutes[i] + '">' + minutes[i] + '</option>';
+  }
+  selectOption = selectOption + '</select>分';
+  $('#event-start-select-minute').append(selectOption);
+  $('#event-end-select-minute').append(selectOption);
+  $('#contact-start-select-minute').append(selectOption);
+  $('#contact-end-select-minute').append(selectOption);
+}*/
