95547e9d3c397ed10b37e43f9846faadc460d5e3
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-27 18:16:56 +0900

    fix position of buttons in header

--- htdocs/header.php
+++ htdocs/header.php
@@ -35,7 +35,7 @@ const openReservationWithNewTab = () => {
 </div>
 <nav class="navbar navbar-expand-lg navbar-light bg-light border-bottom"> <a class="navbar-brand" href="/"> <img class="logo-img header-img" src="img/logo.svg"> </a>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria　expanded="false" aria-label="Toggle navigation"> <span class="navbar-toggler-icon"></span> </button>
-  <div class="collapse navbar-collapse menu" id="navbarSupportedContent">
+  <div class="collapse navbar-collapse menu justify-content-end" id="navbarSupportedContent">
     <ul class="navbar-nav">
         <li class="nav-item"> <a class="nav-link" href="/home.php">Home<br/>
         <span class="menu-japan">ホーム</span></a> </li>
