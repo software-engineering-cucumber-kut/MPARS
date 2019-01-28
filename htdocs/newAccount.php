ca37cd0e24cc26b385c3faed21ee41ac62a9c4f6
Chan <yonezawakyoichirou@gmail.com>
2019-01-28 19:35:23 +0900

    homeへの配置変更

--- htdocs/newAccount.php
+++ htdocs/newAccount.php
@@ -33,7 +33,7 @@
         </div>
 
         <div class="col-sm-8"></div>
-        <div align = "right" class="col-sm-2">
+        <div align = "right" class="col-sm-2" >
         <!--uタグは下線の追加  削除は→text-decoration:none;-->
         <u><a href="home.php" onClick="window.close();" style="margin-top:30px;font-size:30px; ">HOMEへ</a></u>
         </div>
