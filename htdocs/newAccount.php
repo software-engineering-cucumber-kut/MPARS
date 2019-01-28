0c46583ed3893cc2206f7c75b1baf147be462636
Chan <yonezawakyoichirou@gmail.com>
2019-01-28 16:46:16 +0900

    margin変更

--- htdocs/newAccount.php
+++ htdocs/newAccount.php
@@ -33,7 +33,7 @@
         </div>
 
         <div class="col-sm-8"></div>
-        <div align = "right" class="col-sm-2" style="margin-top:30px">
+        <div align = "right" class="col-sm-2" style="margin-top:20px">
         <!--uタグは下線の追加  削除は→text-decoration:none;-->
         <u><a href="home.php" onClick="window.close();" style=" font-size:30px; ">HOMEへ</a></u>
         </div>
