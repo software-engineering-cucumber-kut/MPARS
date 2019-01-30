2200cbe99364f5cacd3da239c8bb6a3b5db012e3
Chan <yonezawakyoichirou@gmail.com>
2019-01-30 14:45:04 +0900

    画像の大きさ変更

--- htdocs/managerHeader.php
+++ htdocs/managerHeader.php
@@ -17,13 +17,11 @@ if (!isset($_SESSION["id"])) {
     <!-- Bootstrapのための各種cssファイル読み込み -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
 
-
     <!-- jQueryのjsファイル読み込み -->
     <script
           src="https://code.jquery.com/jquery-3.3.1.min.js"
           integrity="sha256-FgpCb/KJQlLNfOu91ta32o/NMZxltwRo8QtmkMRdAu8="
           crossorigin="anonymous"></script>
-    <link rel="stylesheet" href="css/header.css">
 
     <!-- jQuery UIを使う -->
     <script src="https://code.jquery.com/ui/1.12.0/jquery-ui.js"></script>
