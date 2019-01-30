aa09458cf4bfe26abc1b235fcd2a351771f40d8c
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-30 15:02:41 +0900

    test mysql connection

--- htdocs/index.php
+++ htdocs/index.php
@@ -1,3 +1,14 @@
 <?php
-require('../vendor/autoload.php');
-require('home.php');
+<?php
+    $url = parse_url(getenv("CLEARDB_DATABASE_URL"));
+
+    $server = $url["host"];
+    $username = $url["user"];
+    $password = $url["pass"];
+    $db = substr($url["path"], 1);
+
+    $link = mysqli_connect($server, $username, $password, $db);
+    $result = mysqli_query($link, "select now()");
+
+    echo var_dump($result);
+?>
