a4a3742269ecc3f1b99e46ab88d67c16140e1cfa
IH-kt <200283r@ugs.kochi-tech.ac.jp>
2019-01-31 10:25:30 +0900

    update stock

--- htdocs/data/putStock.php
+++ htdocs/data/putStock.php
@@ -5,12 +5,31 @@
   require('database.php');
   $json_string = file_get_contents('php://input');
   $json_obj = json_decode($json_string);
+  //var_dump($json_obj);
+  //var_dump($json_obj->stock);
   // $stockInfos = $json_obj->stockinfo;
   // $stockId = database('SELECT', '* FROM stock WHERE id =' . $stockInfo["id"]);
-  $target_string = "stock SET " .
-              			 " currentstock = " . $json_obj->currentstock . " ," .
-              			 " nextstock = " . $json_obj->nextstock . " WHERE id = " . $json_obj->stockId . ";";
-  $dbaccess_result = database('UPDATE', $target_string);
+  $stocks = $json_obj->stock;
+  foreach($stocks as $stock) {
+      //var_dump($stock->itemid);
+      if (database('SELECT', '* FROM stock WHERE itemid = ' . $stock->itemid) == NULL) {
+          $target_string = "INTO stock (itemid, currentstock, nextstock) VALUES(" .
+              $stock->itemid . ", " .
+              $stock->currentstock . ", " .
+              $stock->nextstock . ")";
+          //var_dump($target_string);
+          $dbaccess_result = database('INSERT', $target_string);
+      } else {
+          $target_string = "stock SET " .
+              " currentstock = " . $stock->currentstock . " ," .
+              " nextstock = " . $stock->nextstock . " WHERE itemid = " . $stock->itemid . ";";
+          //var_dump($target_string);
+          $dbaccess_result = database('UPDATE', $target_string);
+      }
+      if (!$dbaccess_result) {
+          break;
+      }
+  }
 	if ($dbaccess_result) {
 		echo "1";
 	} else {
--- htdocs/js/sendStock.js
+++ htdocs/js/sendStock.js
@@ -1,16 +1,32 @@
 const sendStock = () => {
-    let send_data = {};
-    send_data.contents = $('#stock-text').val();
+    let stocks = $('.stock-text');
+    let itemids = stocks.parents('.card-body').find('.itemid');
+    let send_data = {
+        stock:[]
+    };
+    $.each(stocks, (index, stock) => {
+        if (stock.value != "") {
+            send_data.stock.push({
+                itemid: itemids[index].innerText,
+                currentstock: stock.value,
+                nextstock: stock.value
+            });
+        }
+    });
+    console.log(send_data);
     $.ajax({
         url: 'data/putStock.php',
         type: 'POST',
         datatype: 'json',
         data: JSON.stringify(send_data)
     }).done((res) => {
+        console.log(res);
+        /*
         if (JSON.parse(res).success) {
             location.href = "/stock.php";
         } else {
             console.log("request sending failed");
         }
+        */
     });
 };
